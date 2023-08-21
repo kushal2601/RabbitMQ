5a4637e1a86f0c85727af8dfb8608ff0c41dd8ce
Kushal Maradana <Kushal.Maradana@leoforce.local>
2023-08-21 14:06:10 +0530

    Initial commit

---
+++ Send/Send.cs
@@ -0,0 +1,6 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+
+var factory = new ConnectionFactory { HostName = "localhost"};
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
